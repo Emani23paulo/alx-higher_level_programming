819780ed0568c290fa99a2586d0b6e515752620e
Emani23paulo <Emani23paulo@gmail.com>
2023-05-06 07:14:53 -0700

    Updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable 'email' must be sent with the value 'hr@holbertonschool.com'. A variable 'subject' must be sent with the value 'I will always be here for PLD'.
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sd "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
